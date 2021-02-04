e13dbdefc5f99f6f3842db5d26c6bfaa801945ec
Joey <33104951+Jmarcs27@users.noreply.github.com>
2021-02-03 20:35:12 -0500

    Translated sudo-code from Michael and Scott paper

--- src/engine/jobs/ga_queue.cpp
+++ src/engine/jobs/ga_queue.cpp
@@ -16,6 +16,12 @@ ga_queue::ga_queue(int node_count)
 	// For extra credit, preallocate 'node_count' elements (instead of
 	// allocating on push).
 	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+	queue_size = 0;
+	queue = new queue_t();
+	node_t *node = new node_t();
+	queue->head = node, queue->tail = node;
+	queue->h_lock.unlock(), queue->t_lock.unlock();
+
 }
 
 ga_queue::~ga_queue()
@@ -23,6 +29,19 @@ ga_queue::~ga_queue()
 	// TODO:
 	// Free any resources held by the queue.
 	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+	
+	// first locks the head and tail
+	queue->h_lock.lock(), queue->t_lock.lock();
+	while (true)
+	{
+		void* d;
+		// pops from the queue until it is empty
+		if (!pop(&d))
+			break;
+	}
+	queue->h_lock.unlock(), queue->t_lock.unlock();
+	delete(queue);
+	
 }
 
 void ga_queue::push(void* data)
@@ -33,6 +52,15 @@ void ga_queue::push(void* data)
 	// this function is called, you must block until another thread pops an
 	// element off the queue.
 	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
+	node_t *node = new node_t();
+	node->data = data;
+	node->next = NULL;
+	// locks the tail before changing it
+	queue->t_lock.lock();
+	queue->tail->next = node;
+	queue->tail = node;
+	queue->t_lock.unlock();
+	queue_size++;
 }
 
 bool ga_queue::pop(void** data)
@@ -43,12 +71,26 @@ bool ga_queue::pop(void** data)
 	// If the queue is empty when this function is called, return false.
 	// Otherwise return true.
 	// See https://www.research.ibm.com/people/m/michael/podc-1996.pdf
-	return false;
+	
+	// locks the head before changing it
+	queue->h_lock.lock();
+	node_t *node = queue->head;
+	node_t *new_head = node->next;
+	// checks if the queue is empty
+	if (new_head == NULL) {
+		queue->h_lock.unlock();
+		return false;
+	}
+	*data = new_head;
+	queue->head = new_head;
+	queue->h_lock.unlock();
+	free(node);
+	return true;
 }
 
 int ga_queue::get_count() const
 {
 	// TODO:
 	// Get the number of elements currently in the queue.
-	return 0;
+	return queue_size;
 }
--- src/engine/jobs/ga_queue.h
+++ src/engine/jobs/ga_queue.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <mutex>
 
 /*
 ** RPI Game Architecture Engine
@@ -9,6 +10,21 @@
 ** This file is distributed under the MIT License. See LICENSE.txt.
 */
 
+// node structure that stores its data and the next node
+struct node_t
+{
+	void* data;
+	node_t *next;
+};
+// queue structure that 
+struct queue_t
+{
+	node_t *head;
+	node_t *tail;
+	std::mutex h_lock;
+	std::mutex t_lock;
+};
+
 /*
 ** Thread-safe queue.
 ** https://www.research.ibm.com/people/m/michael/podc-1996.pdf
@@ -16,6 +32,12 @@
 class ga_queue
 {
 public:
+	// variables
+	int queue_size;
+	queue_t *queue;
+	
+
+	// functions
 	ga_queue(int node_count);
 	~ga_queue();
 
